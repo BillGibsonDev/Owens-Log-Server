9becc9061d9028808095d7bef4e4b55d049015e8
DevGibby <71801512+DevGibby@users.noreply.github.com>
2023-07-08 19:47:22 -0400

    Update DayController.js

--- controllers/DayController.js
+++ controllers/DayController.js
@@ -5,7 +5,7 @@ import { CronJob } from 'cron';
 export const getDays = async (req, res) => { 
     try {
         const days = await DayModel.find(); 
-        res.status(200).json(days.reverse());
+        res.status(200).json(days.reverse().slice(0,13));
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
@@ -23,16 +23,22 @@ export const getDay = async (req, res) => {
 
 export const createDay = async () => {
     const currentDate = new Date();
-    const newDay = new DayModel({ date: currentDate })
-    try {
-        await newDay.save();
-        return "Day Created!";
-    } catch (error) {
-        return "Error - Day Not Created!";
-    } 
+    const dayCheck = DayModel.find({ date: currentDate });
+    if(!dayCheck){
+        const newDay = new DayModel({ date: currentDate })
+        try {
+            await newDay.save();
+            return "Day Created!";
+        } catch (error) {
+            return "Error - Day Not Created!";
+        } 
+    } else {
+        console.log(dayCheck.date)
+        console.log('Day already created!')
+    }
 }
 
-const dayJob = new CronJob("1 0 * * *", () => {
+const dayJob = new CronJob("03 * * * *", () => {
   createDay();
   console.log('day job started');
 },
